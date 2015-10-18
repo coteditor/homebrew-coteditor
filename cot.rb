69b108c2269e2b545b286769991f5d9dd70fe95d
1024jp <1024jp@wolfrosch.com>
2015-10-19 08:25:36 +0900

    Clean-up brew-file

--- cot.rb
+++ cot.rb
@@ -1,11 +1,14 @@
 class Cot < Formula
-  desc 'The command-line helper tool for CotEditor'
-  homepage 'http://coteditor.com/'
-  url 'https://raw.githubusercontent.com/coteditor/cot/master/cot'
-  sha256 'cda00c591aff5c6113b68e8e12c05a43492d66e5c56a5b745e1a7ce8a5659c1e'
-  version '2.2.0'
+  desc "The command-line helper tool for CotEditor"
+  homepage "http://coteditor.com/"
+  url "https://raw.githubusercontent.com/coteditor/cot/master/cot"
+  version "2.2.0"
+  sha256 "cda00c591aff5c6113b68e8e12c05a43492d66e5c56a5b745e1a7ce8a5659c1e"
 
   def install
-    bin.install 'cot'
+    bin.install "cot"
+  end
+  
+  test do
   end
 end
