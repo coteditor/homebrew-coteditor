941376e4bdc1fd04e63f2f9dc88f6df69a0ffed6
1024jp <1024jp@wolfrosch.com>
2016-02-25 19:17:31 +0900

    use https for homepage address

--- cot.rb
+++ cot.rb
@@ -1,6 +1,6 @@
 class Cot < Formula
   desc "The command-line helper tool for CotEditor"
-  homepage "http://coteditor.com/"
+  homepage "https://coteditor.com/"
   url "https://raw.githubusercontent.com/coteditor/cot/master/cot"
   version "2.3.1"
   sha256 "34e2365681b9f52b6ad9d95bc644ea1560eada4d99cf03cea46eac51a1d03d5c"
