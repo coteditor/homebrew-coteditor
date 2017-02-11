1126c4a443c455c78f62dd31db77230d4704b1ef
1024jp <1024jp@wolfrosch.com>
2017-02-11 18:13:57 +0900

    Update cot version to 2.5.0

--- README.md
+++ README.md
@@ -18,6 +18,6 @@ brew install cot
 License
 -----------------------------
 
-© 2015 1024jp.
+© 2015-2017 1024jp.
 
 The source code is distributed under the terms of the __Apache License, Version 2.0__. See the [LICENSE](LICENSE) for details.
--- cot.rb
+++ cot.rb
@@ -2,8 +2,8 @@ class Cot < Formula
   desc "The command-line helper tool for CotEditor"
   homepage "https://coteditor.com/"
   url "https://raw.githubusercontent.com/coteditor/cot/master/cot"
-  version "2.4.1"
-  sha256 "bb985ae919115be5045862e0115de0879c94fb302139970c77461d929b95592e"
+  version "2.5.0"
+  sha256 "7c5909d8d389a389344e98efc8afceb49dccce7ed552bf74fbabee618255ee77"
 
   def install
     bin.install "cot"
