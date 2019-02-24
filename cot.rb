e09908fbe654fd5f4e5ff2431cc1906bee99be12
Sho Ikeda <suicaicoca@gmail.com>
2019-02-24 21:57:10 +0900

    Fix url in cot.rb
    Should fix https://github.com/coteditor/homebrew-coteditor/issues/1.

--- cot.rb
+++ cot.rb
@@ -1,7 +1,7 @@
 class Cot < Formula
   desc "The command-line helper tool for CotEditor"
   homepage "https://coteditor.com/"
-  url "https://raw.githubusercontent.com/coteditor/cot/master/cot"
+  url "https://raw.githubusercontent.com/coteditor/cot/2.6.2/cot"
   version "2.6.2"
   sha256 "89741c526a88f7e43288e35a4c84d86e303c0b321a47b98b696a65a6fac95a7f"
 
