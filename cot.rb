577a4139084f3b78561941725678f1c833a325f6
1024jp <1024jp@wolfrosch.com>
2021-07-25 11:08:49 +0900

    Update cot version to 2.8.0

--- cot.rb
+++ cot.rb
@@ -2,8 +2,8 @@ class Cot < Formula
   desc "The command-line helper tool for CotEditor"
   homepage "https://coteditor.com/"
   head "https://raw.githubusercontent.com/coteditor/cot/master/cot"
-  url "https://raw.githubusercontent.com/coteditor/cot/2.7.4/cot"
-  sha256 "4d5a058702b6a4a8ffd446cf55907250720596cdc1eee874208ea351ac14a017"
+  url "https://raw.githubusercontent.com/coteditor/cot/2.8.0/cot"
+  sha256 "a585e8ba4b3cb2cf5413b1b8b08e893e28ee47246174c0be79675b71fb2c09dc"
 
   def install
     bin.install "cot"
