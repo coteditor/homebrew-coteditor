759dc10629973dbcfd824de4caeb1a43af6c3c77
1024jp <1024jp@wolfrosch.com>
2022-09-18 11:43:10 +0900

    Update cot.rb

--- cot.rb
+++ cot.rb
@@ -1,8 +1,8 @@
 class Cot < Formula
   desc "The command-line helper tool for CotEditor"
   homepage "https://coteditor.com/"
-  head "https://raw.githubusercontent.com/coteditor/cot/master/cot"
-  url "https://raw.githubusercontent.com/coteditor/cot/2.8.0/cot"
+  head "https://raw.githubusercontent.com/coteditor/cot/main/cot"
+  url "https://raw.githubusercontent.com/coteditor/cot/2.9.2/cot"
   sha256 "a585e8ba4b3cb2cf5413b1b8b08e893e28ee47246174c0be79675b71fb2c09dc"
 
   def install
