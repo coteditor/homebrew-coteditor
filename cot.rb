624fcfa27dbb431351b85e68d9dfd65af6dcd0e5
1024jp <1024jp@wolfrosch.com>
2024-06-04 22:00:16 +0900

    Update cot version to 2.10.0

--- cot.rb
+++ cot.rb
@@ -2,8 +2,8 @@ class Cot < Formula
   desc "The command-line helper tool for CotEditor"
   homepage "https://coteditor.com/"
   head "https://raw.githubusercontent.com/coteditor/cot/main/cot"
-  url "https://raw.githubusercontent.com/coteditor/cot/2.9.2/cot"
-  sha256 "384e3126fc590b1953f73210a7fdeba11517dd561752e566a606f574ced754e7"
+  url "https://raw.githubusercontent.com/coteditor/cot/2.10.0/cot"
+  sha256 "d397d7c65fe0ebe2f26c8e565196b00386599d2e37e1d5a2a2b0fb2c0f968303"
 
   def install
     bin.install "cot"
