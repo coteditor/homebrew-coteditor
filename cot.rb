8e4877a8793f48cf0abb05e78911d9790f2970e1
1024jp <1024jp@wolfrosch.com>
2024-06-04 22:14:28 +0900

    Deprecate cot cask

--- cot.rb
+++ cot.rb
@@ -5,6 +5,8 @@ class Cot < Formula
   url "https://raw.githubusercontent.com/coteditor/cot/2.10.0/cot"
   sha256 "d397d7c65fe0ebe2f26c8e565196b00386599d2e37e1d5a2a2b0fb2c0f968303"
 
+  deprecate! date: "2024-06-04”, because: “has been bundled to the CotEditor.app for years, and therefore, there is no longer any reason to distribute it alone as cask. See: https://coteditor.com/cot”
+
   def install
     bin.install "cot"
   end
